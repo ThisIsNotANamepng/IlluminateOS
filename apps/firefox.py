536191edbb2ce52ca8af33f87430229ec6437a26
ThisIsNotANamepng <74005486+ThisIsNotANamepng@users.noreply.github.com>
2021-05-10 20:40:10 -0500

    Add files via upload

---
+++ apps/firefox.py
@@ -0,0 +1,7 @@
+import webbrowser
+print ("This will take a minute to load, but you will have a fully-functioning copy of firefox in your browser")
+c = webbrowser.get('firefox')
+  
+c.open_new_tab('https://duckduckgo.com/')
+
+import main
