6087425f09e7b0e9e341483a4c5eab8ac246be6a
ThisIsNotANamepng <74005486+ThisIsNotANamepng@users.noreply.github.com>
2021-05-10 20:53:47 -0500

    Update attest.py

--- osapps/attest.py
+++ osapps/attest.py
@@ -15,9 +15,12 @@ mainsdd = a.read()
 h_obj = SHA3_512.new()
 h_obj.update(bytes(mainsdd, 'utf-8'))
 hhbkvb = (h_obj.hexdigest())
-if hhbkvb != "8eacd2da6f570225df48980fe8d529980195111a5ac31af01532d6776c761fc1136c9e9946f2327bb8b50c662e0652de44e7ae1663c13c3207d5adc075e9e78e":
+if hhbkvb != "ffe3ed5fbada9b8c12efe51270d2688c2d0a2715549941efb752e27995f104599563e734dd14532b1e7b47cf23a46f10c890dc1f3d900356f5553e37649b1b60":
   print ("The OS has been modified. Shutting down...")
+  print (hhbkvb)
   #exit()
+if hhbkvb == 'ffe3ed5fbada9b8c12efe51270d2688c2d0a2715549941efb752e27995f104599563e734dd14532b1e7b47cf23a46f10c890dc1f3d900356f5553e37649b1b60':
+  print ("The OS is validated\n")
 
 
 
